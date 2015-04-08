c6e0c9d0806a9445a4f25e38d4552145bf4714f2
Scott Willson <scott.willson@gmail.com>
2015-04-08 08:27:00 -0700

    Replace hardcoded JSON string with function + encoding

--- src/play_by_play/app_server/handler.clj
+++ src/play_by_play/app_server/handler.clj
@@ -1,20 +1,24 @@
 (ns play-by-play.app-server.handler
-  (:require [play-by-play.app-server.logging :refer :all]
+  (:require [play-by-play.season :as season]
+            [play-by-play.app-server.logging :refer :all]
             [ring.middleware.stacktrace :refer :all]
             [ring.middleware.reload :refer :all]
             [ring.util.response :as resp]
             [compojure.core :refer :all]
             [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [cheshire.core :as cheshire]
+            [clojure.string :as str]))
+
+(defn to-camel-case [k]
+  (let [s (if (keyword? k) (name k) k)]
+    (str/replace s #"\-[A-z]" (fn [[dash letter]]
+                            (.toUpperCase (str letter))))))
 
 (defn index-json [request]
   {:status 200
    :headers {"Content-Type" "application/json"}
-   :body "[{
-     \"homeTeam\": \"New Orleans\",
-     \"homeScore\": 100,
-     \"visitorTeam\": \"New York\",
-     \"visitorScore\": 99}]"})
+   :body (cheshire/encode season/day {:key-fn (fn [k] (to-camel-case (name k)))})})
 
 (defroutes app-routes
   (GET "/index.json" [] index-json)
