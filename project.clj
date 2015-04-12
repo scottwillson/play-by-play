a001384dfa2f0789fc994520578987b962c77861
Scott Willson <scott.willson@gmail.com>
2015-04-12 11:38:20 -0700

    Try quickie for auto test running

--- project.clj
+++ project.clj
@@ -9,19 +9,20 @@
                  [cheshire "5.4.0"]
                  [clj-time "0.9.0"]
                  [incanter "1.5.6"]
-                 [lein-light-nrepl "0.1.0"]
                  [ring "1.3.2"]
                  [compojure "1.3.2"]]
   :main ^:skip-aot play-by-play.core
   :target-path "target/%s"
-  :plugins [[lein-ring "0.9.3"]]
+  :plugins [[lein-ring "0.9.3"] [quickie "0.3.9"]]
   :repl-options {:nrepl-middleware [lighttable.nrepl.handler/lighttable-ops]}
   :ring {:handler play-by-play.app-server.handler/app}
   :profiles
     {:uberjar {:aot :all}
-     :dev     {:dependencies [[ring-mock "0.1.5"]
+     :dev     {:dependencies [[lein-light-nrepl "0.1.0"]
+                              [ring-mock "0.1.5"]
                               [org.mortbay.jetty/jetty "6.1.26"]
                               [clj-webdriver "0.6.1"]]}}
   :test-selectors {:default (complement :browser)
                    :browser :browser
-                   :all     (constantly true)})
+                   :all     (constantly true)}
+  :test-matcher #".*^(browser)")
