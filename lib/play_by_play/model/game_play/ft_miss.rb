ed58c81421ced6702f353d5d0fddd2584329163f
Scott Willson <scott.willson@gmail.com>
2016-04-24 13:24:06 -0700

    Don't set ball_in_play to false when it is laready false

--- lib/play_by_play/model/game_play/ft_miss.rb
+++ lib/play_by_play/model/game_play/ft_miss.rb
@@ -5,16 +5,16 @@ module PlayByPlay
         attributes = decrement_free_throws(possession)
 
         if possession.last_free_throw? && possession.next_team
-          attributes.merge(ball_in_play: false, next_team: nil, team: possession.next_team)
+          attributes.merge(next_team: nil, team: possession.next_team)
 
         elsif possession.last_free_throw?
           attributes.merge(ball_in_play: true, next_team: nil, team: nil)
 
         elsif possession.last_technical_free_throw? && possession.free_throws?
-          attributes.merge(ball_in_play: false, team: possession.free_throws.last)
+          attributes.merge(team: possession.free_throws.last)
 
         elsif possession.last_technical_free_throw? && !possession.free_throws?
-          attributes.merge(ball_in_play: false, next_team: nil, team: possession.next_team)
+          attributes.merge(next_team: nil, team: possession.next_team)
 
         elsif attributes[:technical_free_throws]
           attributes.merge(team: attributes[:technical_free_throws].last)
