9e204dec88bc65cbe6cd25f6b194a14a16f5361d
Scott Willson <scott.willson@gmail.com>
2016-11-05 11:51:19 -0700

    Pull offense/defense home/visitor probabilities separately for better caching

---
+++ lib/play_by_play/views/game.rb
@@ -0,0 +1,17 @@
+module PlayByPlay
+  module Views
+    class Game
+      def initialize(game)
+        @game = game
+      end
+
+      def team_text(team)
+        team.abbreviation || team.key
+      end
+
+      def to_s
+        "#{team_text(@game.visitor)} #{@game.possession.visitor.points}\n#{team_text(@game.home)} #{@game.possession.home.points}\n"
+      end
+    end
+  end
+end
